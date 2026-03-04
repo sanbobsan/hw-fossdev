9259ae436b4b0dddbf40b304875870f16daa3a98
sanbob <saintsanbob@gmail.com>
2026-03-04 16:56:39 +0800

    chore: add tests
    - add addition commutative test
    - add tax calculation test

--- classwork/testing/test/test_principles.py
+++ classwork/testing/test/test_principles.py
@@ -1,12 +1,35 @@
-
 from app.math_demo import add
+from app.math_demo import calculate_tax
+
 
 def test_addition() -> None:
     assert add(2, 2) == 4
-    print("Test succed")
+    assert add(-10, 16) == 6
+    assert add(-500, 250) == -250
+    print("Test ADDITION PASSED")
+
+
+def test_addition_commutative() -> None:
+    assert add(-5, 12) == add(12, -5)
+    assert add(100, -200) == add(-200, 100)
+    print("Test ADDITION COMMUTATIVE PASSED")
+
+
+def test_tax_calculation():
+    assert calculate_tax(1000) == 150.0
+    assert calculate_tax(100) == 15.0
+    assert calculate_tax(10) == 1.5
+    assert calculate_tax(1) == 0.15
+    assert calculate_tax(-200) == -30
+    assert calculate_tax(0) == 0
+    print("Test TAX CALCULATION PASSED")
+
 
 def main() -> None:
     test_addition()
+    test_addition_commutative()
+    test_tax_calculation()
+
 
 if __name__ == "__main__":
     main()
