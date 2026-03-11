76ffb75b2529bdd1d6a94889300aa2690e4b81b3
sanbob <saintsanbob@gmail.com>
2026-03-11 17:00:33 +0800

    feat: improve ndfl calculating

--- classwork/testing_tdd/src/ndfl/main.py
+++ classwork/testing_tdd/src/ndfl/main.py
@@ -1,2 +1,15 @@
 def calculate_ndfl_tax(income: float) -> float:
-    return income * 0.13
+    result = 0.0
+
+    if income >= 5_000_000:
+        result += (income - 5_000_000) * 0.18
+        income = 5_000_000
+
+    if income >= 2_400_000:
+        result += (income - 2_400_000) * 0.15
+        income = 2_400_000
+
+    if income:
+        result += income * 0.13
+
+    return result
