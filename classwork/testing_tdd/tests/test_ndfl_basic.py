0539104acd37499333b88d995fbf7d221d42e320
sanbob <saintsanbob@gmail.com>
2026-03-11 17:01:54 +0800

    test: fix test for tier 2

--- classwork/testing_tdd/tests/test_ndfl_basic.py
+++ classwork/testing_tdd/tests/test_ndfl_basic.py
@@ -1,16 +1,12 @@
 from ndfl.main import calculate_ndfl_tax
 
 
-def test_ndfl():
-    assert calculate_ndfl_tax() is None
-
-
 def test_ndfl_tier_1():
     assert calculate_ndfl_tax(500_000) == 65_000
 
 
 def test_ndfl_tier_2():
-    assert calculate_ndfl_tax(4_000_000) == 512_000
+    assert calculate_ndfl_tax(4_000_000) == 552_000
 
 
 def test_ndfl_tier_3():
