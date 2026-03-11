299f73d9bd6039fe0fb7189c48a64f2a4896441e
sanbob <saintsanbob@gmail.com>
2026-03-11 16:43:40 +0800

    test: add tests for basic ndfl

--- classwork/testing_tdd/tests/test_ndfl_basic.py
+++ classwork/testing_tdd/tests/test_ndfl_basic.py
@@ -3,3 +3,18 @@ from ndfl.main import calculate_ndfl_tax
 
 def test_ndfl():
     assert calculate_ndfl_tax() is None
+
+
+def test_ndfl_tier_1():
+    assert calculate_ndfl_tax(500_000) == 65_000
+
+
+def test_ndfl_tier_2():
+    assert calculate_ndfl_tax(4_000_000) == 512_000
+
+
+def test_ndfl_tier_3():
+    assert calculate_ndfl_tax(10_000_000) == 1_602_000
+
+
+# TODO: make last tiers
